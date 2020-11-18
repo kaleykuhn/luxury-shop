de605bf1df008f89c0b2adfe6e9afafa77d559b4
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-11-17 21:39:58 -0800

    add HomeScreen Product Listing

---
+++ frontend/src/products.js
@@ -0,0 +1,163 @@
+// const products = [
+//   {
+//     _id: '1',
+//     name: 'Airpods Wireless Bluetooth Headphones',
+//     image: '/images/airpods.jpg',
+//     description:
+//       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+//     brand: 'Apple',
+//     category: 'Electronics',
+//     price: 89.99,
+//     countInStock: 10,
+//     rating: 4.5,
+//     numReviews: 12,
+//   },
+//   {
+//     _id: '2',
+//     name: 'iPhone 11 Pro 256GB Memory',
+//     image: '/images/phone.jpg',
+//     description:
+//       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+//     brand: 'Apple',
+//     category: 'Electronics',
+//     price: 599.99,
+//     countInStock: 7,
+//     rating: 4.0,
+//     numReviews: 8,
+//   },
+//   {
+//     _id: '3',
+//     name: 'Cannon EOS 80D DSLR Camera',
+//     image: '/images/camera.jpg',
+//     description:
+//       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+//     brand: 'Cannon',
+//     category: 'Electronics',
+//     price: 929.99,
+//     countInStock: 5,
+//     rating: 3,
+//     numReviews: 12,
+//   },
+//   {
+//     _id: '4',
+//     name: 'Sony Playstation 4 Pro White Version',
+//     image: '/images/playstation.jpg',
+//     description:
+//       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+//     brand: 'Sony',
+//     category: 'Electronics',
+//     price: 399.99,
+//     countInStock: 11,
+//     rating: 5,
+//     numReviews: 12,
+//   },
+//   {
+//     _id: '5',
+//     name: 'Logitech G-Series Gaming Mouse',
+//     image: '/images/mouse.jpg',
+//     description:
+//       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+//     brand: 'Logitech',
+//     category: 'Electronics',
+//     price: 49.99,
+//     countInStock: 7,
+//     rating: 3.5,
+//     numReviews: 10,
+//   },
+//   {
+//     _id: '6',
+//     name: 'Amazon Echo Dot 3rd Generation',
+//     image: '/images/alexa.jpg',
+//     description:
+//       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+//     brand: 'Amazon',
+//     category: 'Electronics',
+//     price: 29.99,
+//     countInStock: 0,
+//     rating: 4,
+//     numReviews: 12,
+//   },
+// ]
+
+// export default products;
+const products = [
+  {
+    _id: '1',
+    name: 'Tory Small Bag  ',
+    image: '/images/tory4.jpg',
+    description:
+      'Our Lee Radziwill Small Bag is impeccably crafted in croc-embossed Italian leather. Detailed with signature lock-and-key hardware, the frame bag comes with an optional shoulder strap. The collection is named for Lee Radziwill, a woman Tory always admired — she was fearlessly unique ',
+    brand: 'Lee Radziwill',
+    category: 'Luxurybags',
+    price: 798.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Paloma Fold-Over Clutch Bag',
+    image: '/images/christian-Louboutin.jpg',
+    description: 'Grained calfskin clutch bag with mixed studs',
+    brand: 'Christian Louboutin',
+    category: 'Luxurybags',
+    price: 1450.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Tess Small Shouler Bag',
+    image: '/images/chloe.jpg',
+    description:
+      'designed in a small saddle shape that pays homage to the equestrian roots. Made in Italy from smooth black leather and supple suede, it has a ring-embellished front flap and compact interior with enough space for your phone, keys and wallet. The dual straps allow for a variety of carrying options.',
+    brand: 'Chloe',
+    category: 'Luxurybags',
+    price: 1890,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Dionysus GG Supreme Chain Wallet',
+    image: '/images/gucci.jpg',
+    description:
+      'Removable chain shoulder strap with a snap-flap closure and tiger head spur. Antiqued silertone hardware with leather lining',
+    brand: 'Gucci',
+    category: 'Luxurybags',
+    price: 1300,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Reversible Liz Shopper in Visetos',
+    image: '/images/tanpurse.jpg',
+    description:
+      'Crafted from classic Visetos, this practical style has a spacious silhouette that can be reversed and adjusted using side drawstrings. For unrivaled versatility, it also comes with a removable pouch that can be used as a separate purse.',
+    brand: 'MCM',
+    category: 'Luxurybags',
+    price: 735.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Monogram YSL Envelope Small Chain Sholder Bag',
+    image: '/images/saintlaurent.jpg',
+    description:
+      'Saint Laurent shoulder bag in quilted powder-grain calf leather. Flap top with signature YSL logo; snap closure',
+    brand: 'Saint Laurent',
+    category: 'Luxurybags',
+    price: 1999.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
