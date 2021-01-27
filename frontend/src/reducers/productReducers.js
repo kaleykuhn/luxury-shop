9844c993967b3df00c0a1aa3bd10c2f32c104180
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2021-01-27 14:51:29 -0800

    add backend user auth

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -11,9 +11,6 @@ export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
-      {
-        /*product*/
-      }
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
