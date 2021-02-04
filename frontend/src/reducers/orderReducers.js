349bc4d34696d236cc3fa05cc1089f1395ef96d7
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2021-02-04 15:10:45 -0800

    add /  route actions orderDetailsReducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -30,7 +30,7 @@ export const orderCreateReducer = (state = {}, action) => {
 };
 
 export const orderDetailsReducer = (
-  state = { orderItems: [], shippingAdress: {} },
+  state = { loading: true, orderItems: [], shippingAdress: {} },
   action
 ) => {
   switch (action.type) {
