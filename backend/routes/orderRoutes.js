3ef937bc34b1adbeb152b8aafcf4a41d9d5f6bd0
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2021-02-05 00:38:58 -0800

    add update to paid endpoint and Pay red/action

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
 //make sure this is at the bottom line of routes or it will try by id
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -28,7 +28,7 @@ const OrderScreen = ({ match }) => {
     if (!order || order._id !== orderId) {
       dispatch(getOrderDetails(orderId));
     }
-  }, [order, orderId]);
+  }, [dispatch, orderId, order]);
 
   return loading ? (
     <Loader />
