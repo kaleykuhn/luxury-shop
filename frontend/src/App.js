f59b0c0471e4c08eeb4f32f32b9ff42200648b49
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2021-02-16 01:44:28 -0800

    Prepare For Deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,10 +18,12 @@ import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       <main className='py-3'>
         <Container>
